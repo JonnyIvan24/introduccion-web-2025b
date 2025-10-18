b1150f13367078ca49a12ec50dc4eb1569f86a6c
JonnyIvan24 <johnny.perez.u@gmail.com>
2025-10-18 10:04:09 -0600

    Organizar código de js en archivos separados

---
+++ src/js/condicionales.js
@@ -0,0 +1,17 @@
+var edad = Number(prompt("ingresa tu edad"))
+
+if (edad > 20) {
+    console.log("eres mayor de 20 años")
+} else if (edad === 20) {
+    console.log("Tienes 20 años")
+} else {
+    console.log("Eres menor de 20 años")
+}
+
+if (edad >= 0 && edad < 20) {
+    console.log("eres menor de 20 años")
+} else if (edad >= 20 && edad <= 40) {
+    console.log("Tienes entre 20 y 40 años")
+} else {
+    console.log("Eres mayor de 40 años")
+}
